fa27b8c29a127111113e59a837fc3226590a0ab0
Nick Lloyd <nicholas.lloyd@pearson.com>
2013-09-27 15:27:52 -0400

    fixes after testing this with a collectd instance

--- mongodb.py
+++ mongodb.py
@@ -11,7 +11,7 @@ from copy import deepcopy
 
 # maps that duplicate the key/nested-key structure for returned data from mongodb commands but mapping
 # to values that are a tuple containing the value's collectd type (types.db)
-# and optionally the collectd value name (defaults to nested key path to the leaf value with key strings joined with '.')
+# and optionally the collectd value name (defaults to nested key path to the leaf value with key strings joined with ".")
 # NOTE: some metrics are more complicated and thus are handled specially (ex. connPoolStats hosts data)
 CONNECTION_POOL_STATUS_METRICS = {
     # db.runCommand({connPoolStats:1})
@@ -21,49 +21,49 @@ CONNECTION_POOL_STATUS_METRICS = {
         "set": "total_connections",
         "sync": "total_connections"
     },
-    "totalAvailable": "guage",
-    'totalCreated': "total_connections",
-    'numDBClientConnection': "guage",
-    'numAScopedConnection': "guage"
+    "totalAvailable": "gauge",
+    "totalCreated": "total_connections",
+    "numDBClientConnection": "gauge",
+    "numAScopedConnection": "gauge"
 }
 
 DBSTATS_METRICS = {
     # db.runCommand({dbStats:1})
-    "collections": "guage",
-    "objects" : "guage",
+    "collections": "gauge",
+    "objects" : "gauge",
     "avgObjSize" : "bytes",
     "dataSize" : "bytes",
     "storageSize" : "bytes",
-    "numExtents" : "guage",
-    "indexes" : "guage",
+    "numExtents" : "gauge",
+    "indexes" : "gauge",
     "indexSize" : "bytes",
     "fileSize" : "bytes",
-    "nsSizeMB": "guage"
+    "nsSizeMB": "gauge"
 }
 
 SERVER_STATUS_METRICS = {
     # db.runCommand({serverStatus:1})
     "backgroundFlushing": {
-        "last_ms": "guage" 
+        "last_ms": "gauge" 
     },
     "connections": {
-        "current": "guage",
-        "available": "guage"
+        "current": "gauge",
+        "available": "gauge"
     },
     "cursors": {
-        "totalOpen": "guage",
+        "totalOpen": "gauge",
         "timedout": "derive"
     },
     "globalLock": {
         "currentQueue" : {
-            "total" : "guage",
-            "readers" : "guage",
-            "writers" : "guage"
+            "total" : "gauge",
+            "readers" : "gauge",
+            "writers" : "gauge"
         },
         "activeClients" : {
-            "total" : "guage",
-            "readers" : "guage",
-            "writers" : "guage"
+            "total" : "gauge",
+            "readers" : "gauge",
+            "writers" : "gauge"
         }
     },
     "indexCounters" : {
@@ -98,11 +98,11 @@ SERVER_STATUS_METRICS = {
         # do we want db specificity?
     },
     "mem": {
-        "bits" : "guage",
-        "resident" : "guage",
-        "virtual" : "guage",
-        "mapped" : "guage",
-        "mappedWithJournal" : "guage"
+        "bits" : "gauge",
+        "resident" : "gauge",
+        "virtual" : "gauge",
+        "mapped" : "gauge",
+        "mappedWithJournal" : "gauge"
     },
     "metrics" : {
         "document" : {
@@ -137,12 +137,12 @@ class MongoDB(object):
         if not self.mongo_client.alive():
             collectd.error("mongodb plugin failed to connect to %s:%d" % (self.mongo_host, self.mongo_port))
 
-        server_status = self.mongo_client[self.mongo_dbs[0]].command('serverStatus')
-        version = server_status['version']
-        at_least_2_4 = V(version) >= V('2.4.0')
+        server_status = self.mongo_client[self.mongo_dbs[0]].command("serverStatus")
+        version = server_status["version"]
+        at_least_2_4 = V(version) >= V("2.4.0")
         if not at_least_2_4:
-            indexCountersMap = SERVER_STATUS_METRICS.pop('indexCounters')
-            SERVER_STATUS_METRICS['indexCounters'] = {'btree': indexCountersMap}
+            indexCountersMap = SERVER_STATUS_METRICS.pop("indexCounters")
+            SERVER_STATUS_METRICS["indexCounters"] = {"btree": indexCountersMap}
 
 
     def disconnect(self):
@@ -152,21 +152,21 @@ class MongoDB(object):
 
     def config(self, obj):
         for node in obj.children:
-            if node.key == 'Port':
+            if node.key == "Port":
                 self.mongo_port = int(node.values[0])
-            elif node.key == 'Host':
+            elif node.key == "Host":
                 self.mongo_host = node.values[0]
-            elif node.key == 'User':
+            elif node.key == "User":
                 self.mongo_user = node.values[0]
-            elif node.key == 'Password':
+            elif node.key == "Password":
                 self.mongo_password = node.values[0]
-            elif node.key == 'Databases':
+            elif node.key == "Databases":
                 self.mongo_dbs = node.values
-            elif node.key == 'ConnectionPoolStatus':
+            elif node.key == "ConnectionPoolStatus":
                 self.includeConnPoolMetrics = node.values
-            elif node.key == 'ServerStats':
+            elif node.key == "ServerStats":
                 self.includeServerStatsMetrics = node.values
-            elif node.key == 'DBStats':
+            elif node.key == "DBStats":
                 self.includeDbstatsMetrics = node.values
             else:
                 collectd.warning("mongodb plugin: Unkown configuration key %s" % node.key)
@@ -176,7 +176,7 @@ class MongoDB(object):
         # actually a recursive submit call to dive deeper into nested dict data
         # since the leaf value in the nested dicts is the type, we check on the type type :-)
         if db:
-            plugin_instance = '%s-%d' % (self.mongo_port, db)
+            plugin_instance = "%s-%d" % (self.mongo_port, db)
         else:
             plugin_instance = str(self.mongo_port)
         v = collectd.Values()
@@ -194,7 +194,7 @@ class MongoDB(object):
             for type_name, type_value in type_tree.iteritems():
                 next_instance_name = None
                 if instance_name:
-                    next_instance_name = instance_name + '.' + type_name
+                    next_instance_name = instance_name + "." + type_name
                 else:
                     next_instance_name = type_name
                 if data_tree.has_key(type_name):
@@ -203,7 +203,7 @@ class MongoDB(object):
                     # may want to log this but some mongodb setups may not have anything to report
                     pass
         elif isinstance(type_tree, types.DictType) or isinstance(data_tree, types.DictType):
-            print('type tree and data tree structure differ for data instance: ' + instance_name)
+            print("type tree and data tree structure differ for data instance: " + instance_name)
         else:
             self.submit(instance_name, type_tree, data_tree, db)
 
@@ -214,7 +214,7 @@ class MongoDB(object):
         if self.mongo_user and self.mongo_password:
             db.authenticate(self.mongo_user, self.mongo_password)
 
-        conn_pool_stats = db.command('connPoolStats')
+        conn_pool_stats = db.command("connPoolStats")
         metrics_to_collect = {}
         if self.includeConnPoolMetrics:
             for root_metric_key in self.includeConnPoolMetrics.iterkeys():
@@ -232,7 +232,7 @@ class MongoDB(object):
             if self.mongo_user and self.mongo_password:
                 db.authenticate(self.mongo_user, self.mongo_password)
 
-            dbstats = db.command('dbStats')
+            dbstats = db.command("dbStats")
             metrics_to_collect = {}
             if self.includeDbstatsMetrics:
                 for root_metric_key in self.includeDbstatsMetrics.iterkeys():
@@ -250,7 +250,7 @@ class MongoDB(object):
         if self.mongo_user and self.mongo_password:
             db.authenticate(self.mongo_user, self.mongo_password)
 
-        server_status = db.command('serverStatus')
+        server_status = db.command("serverStatus")
         metrics_to_collect = {}
         if self.includeServerStatsMetrics:
             for root_metric_key in self.includeServerStatsMetrics.iterkeys():
@@ -258,23 +258,27 @@ class MongoDB(object):
                     metrics_to_collect[root_metric_key] = deepcopy(SERVER_STATUS_METRICS[root_metric_key])
         else:
             metrics_to_collect = deepcopy(SERVER_STATUS_METRICS)
-        # rename '.' lock to be 'GLOBAL'
-        if metrics_to_collect['locks'].has_key('.'):
-            print(SERVER_STATUS_METRICS['locks'])
-            global_lock_data = metrics_to_collect['locks'].pop('.')
-            metrics_to_collect['locks']['GLOBAL'] = global_lock_data
+        # rename "." lock to be "GLOBAL"
+        if metrics_to_collect["locks"].has_key("."):
+            print(SERVER_STATUS_METRICS["locks"])
+            global_lock_data = metrics_to_collect["locks"].pop(".")
+            metrics_to_collect["locks"]["GLOBAL"] = global_lock_data
 
-            print(SERVER_STATUS_METRICS['locks'])
+            print(SERVER_STATUS_METRICS["locks"])
         for db_name in self.mongo_dbs:
-            metrics_to_collect['locks'][db_name] = deepcopy(SERVER_STATUS_METRICS['locks']['.'])
+            metrics_to_collect["locks"][db_name] = deepcopy(SERVER_STATUS_METRICS["locks"]["."])
 
         self.recursive_submit(metrics_to_collect, server_status)
 
 
+    def publish_data(self):
+        self.publish_server_status()
+        self.publish_connection_pool_metrics()
+        self.publish_dbstats()
+
+
 mongodb = MongoDB()
-collectd.register_read(mongodb.publish_connection_pool_metrics)
-collectd.register_read(mongodb.publish_dbstats)
-collectd.register_read(mongodb.publish_server_status)
+collectd.register_read(mongodb.publish_data)
 collectd.register_config(mongodb.config)
 collectd.register_init(mongodb.connect)
 collectd.register_shutdown(mongodb.disconnect)
